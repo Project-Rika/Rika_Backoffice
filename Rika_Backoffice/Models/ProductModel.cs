4fc957cc20c8722bcdbc227800ab8940a1001d6b
Felix Gammal <felixgammal1@hotmail.com>
2024-11-01 14:00:59 +0100

    Added CreateProductView, function to create a product and tested it. Adds a product to DB if Id is also filled in, needs to be fixed

---
+++ Rika_Backoffice/Models/ProductModel.cs
@@ -0,0 +1,29 @@
+﻿namespace Rika_Backoffice.Models;
+
+public class ProductModel
+{
+    public string? Id { get; set; } 
+    public string ProductName { get; set; } = null!;
+    public decimal ProductPrice { get; set; } = 0;
+    public decimal ProductSalePrice { get; set; } = 0;
+    public string ProductDescription { get; set; } = null!;
+    public int ProductCategoryId { get; set; }
+
+    public int ProductSizeId { get; set; }
+    public ProductSizeEntity ProductSize { get; set; } = null!;
+
+    public int? ProductColorId { get; set; }
+    public ProductColorEntity? ProductColor { get; set; }
+}
+
+public class ProductColorEntity
+{
+    public int Id { get; set; }
+    public string ColorName { get; set; } = null!;
+}
+
+public class ProductSizeEntity
+{
+    public int Id { get; set; }
+    public string ProductSizeName { get; set; } = null!;
+}
