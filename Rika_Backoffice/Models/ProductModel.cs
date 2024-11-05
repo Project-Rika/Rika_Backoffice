5b67aefd67c9851b7a532948c41fc94c46665880
Felix Gammal <felixgammal1@hotmail.com>
2024-11-05 15:10:38 +0100

    Added drop-down menu for colors when creating a product

--- Rika_Backoffice/Models/ProductModel.cs
+++ Rika_Backoffice/Models/ProductModel.cs
@@ -1,8 +1,7 @@
 ﻿namespace Rika_Backoffice.Models;
 
-public class ProductModel
+public class CreateProductModel
 {
-    public string? Id { get; set; } 
     public string ProductName { get; set; } = null!;
     public decimal ProductPrice { get; set; } = 0;
     public decimal ProductSalePrice { get; set; } = 0;
@@ -16,10 +15,27 @@ public class ProductModel
     public ProductColorEntity? ProductColor { get; set; }
 }
 
+public class  ProductModel
+{
+    public string Id { get; set; } = null!;
+    public string ProductName { get; set; } = null!;
+    public decimal ProductPrice { get; set; } = 0;
+    public decimal ProductSalePrice { get; set; } = 0;
+    public string ProductDescription { get; set; } = null!;
+    public int ProductCategoryId { get; set; }
+
+    public int ProductSizeId { get; set; }
+    public ProductSizeEntity ProductSize { get; set; } = null!;
+
+    public int? ProductColorId { get; set; }
+    public ProductColorEntity? ProductColor { get; set; }
+}
+
+
 public class ProductColorEntity
 {
-    public int Id { get; set; }
-    public string ColorName { get; set; } = null!;
+    public int id { get; set; }
+    public string colorName { get; set; } = null!;
 }
 
 public class ProductSizeEntity
