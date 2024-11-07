c2da2cff6fc1c19294f2afa65abc405fcab653fb
Hampus Holmberg <hampusholmberg@LIVE.COM>
2024-11-07 16:04:04 +0100

    Progress on GetOne, now functional

--- Rika_Backoffice/Models/ProductModel.cs
+++ Rika_Backoffice/Models/ProductModel.cs
@@ -23,6 +23,7 @@ public class ProductModel
     public decimal ProductSalePrice { get; set; } = 0;
     public string ProductDescription { get; set; } = null!;
     public int ProductCategoryId { get; set; }
+    public CategoryEntity? ProductCategory { get; set; }
 
     public int ProductSizeId { get; set; }
     public ProductSizeEntity ProductSize { get; set; } = null!;
@@ -42,4 +43,12 @@ public class ProductSizeEntity
 {
     public int Id { get; set; }
     public string ProductSizeName { get; set; } = null!;
+}
+
+public class CategoryEntity
+{
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+
 }
